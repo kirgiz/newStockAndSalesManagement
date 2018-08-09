4a4d5d82da99ab065ef4a70d78f8b08da3611ff9
vagrant <vagrant@vagrant.vm>
2018-08-09 13:22:05 +0000

    Deploy to Heroku

--- src/main/webapp/app/entities/materialhistory-stock-and-sales-utility/material-search-stock-and-sales-utility-dialog.component.ts
+++ src/main/webapp/app/entities/materialhistory-stock-and-sales-utility/material-search-stock-and-sales-utility-dialog.component.ts
@@ -25,6 +25,7 @@ export class MaterialSearchStockAndSalesUtilityDialogComponent implements OnInit
     isSaving: boolean;
 
     materials: MaterialStockAndSalesUtility[];
+    selectedMaterial: number[];
 
     transferclassifications: TransferclassificationStockAndSalesUtility[];
 
@@ -40,6 +41,7 @@ export class MaterialSearchStockAndSalesUtilityDialogComponent implements OnInit
     predicate: any;
     previousPage: any;
     reverse: any;
+    searchfield: string;
 
     constructor(
         public activeModal: NgbActiveModal,
